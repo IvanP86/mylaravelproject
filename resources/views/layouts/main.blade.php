c7c229358a64014b2487351e0b9624dfe10fee44
IvanP <inthedark86@yandex.ru>
2023-01-10 17:28:55 +0500

    'next'

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,19 +3,38 @@
 <head>
   <meta charset="utf-8" />
   <title></title>
-  
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-<div>
-<div>
-	<nav>
+<div class="container">
+<div class="row">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav mr-auto">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="{{ route('main.index') }}">Main <span class="sr-only">Posts</span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+	      </li>
+
+	      <li class="nav-item">
+	        <a class="nav-link" href="{{ route('about.index') }}">About</a>
+	      </li>
+          <li class="nav-item">
+	        <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+<!-- 	<nav>
 		<ul>
 			<li><a href="{{ route('main.index') }}">Main</a></li>
 			<li><a href="{{ route('post.index') }}">Posts</a></li>
 			<li><a href="{{ route('about.index') }}">About</a></li>
 			<li><a href="{{ route('contact.index') }}">Contacts</a></li>
 		</ul>
-	</nav>
+	</nav> -->
 </div>
 	@yield('content')
 
