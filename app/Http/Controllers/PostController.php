66fba92244c286a7a5c358459f28d10beeb31721
IvanP86 <inthedark86@yandex.ru>
2023-01-24 14:07:35 +0500

    CRUD interface 4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -70,7 +70,7 @@ class PostController extends Controller
     public function store()
     {
     	$data = request()->validate([
-    		'title' => 'string',
+    		'title' => 'required|string',
     		'content' => 'string',
     		'image' => 'string',
             'category_id' => '',
