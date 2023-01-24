4c3d6c537904ae947d600ecc6a7be1d7dd100b60
IvanP86 <inthedark86@yandex.ru>
2023-01-24 17:26:16 +0500

    Request class

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -11,15 +11,9 @@ use App\Controller;
 
 class StoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-    	$data = request()->validate([
-    		'title' => 'required|string',
-    		'content' => 'string',
-    		'image' => 'string',
-            'category_id' => '',
-            'tags' => ''
-    		]);
+    	$data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
     	$post = Post::create($data);
--- app/Http/Controllers/Post/UpdateController.php
+++ app/Http/Controllers/Post/UpdateController.php
@@ -11,15 +11,9 @@ use App\Controller;
 
 class UpdateController extends Controller
 {
-    public function __invoke(Post $post)
+    public function __invoke(UpdateRequest $request, Post $post)
     {
-        $data = request()->validate([
-            'title' => 'string',
-            'content' => 'string',
-            'image' =>'string',
-            'category_id' => '',
-            'tags' => ''
-            ]);
+        $data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
         $post->update($data);
